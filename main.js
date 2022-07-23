d14db5387aa4aa445d3469ca442f5c3c2a200635
earthbender159 <80174685+earthbender159@users.noreply.github.com>
2022-07-23 14:14:02 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 song1 = ""; 
 song2 = ""; 
-song1_status = ""; 
+song1_status = "";
+song2_status = "";  
 leftWristX = 0; 
 leftWristY = 0; 
 scoreleftWrist = 0; 
+scorerightWrist = 0; 
 rightWristX = 0; 
 rightWristY = 0;
 
@@ -35,15 +37,36 @@ function draw(){
     stroke("#FF0000");  
     song1_status = song1.isPlaying(); 
 
-    if(song1_status == false){
+    if(song1_status == true){
         song1.play(); 
-        document.getElementById("song_name").innerHTML = "playing Heart_Of_Glass_(getmp3.pro).mp3"; 
+        document.getElementById("song_name").innerHTML = "playing Sugar_(getmp3.pro).mp3"; 
+    }
+    if(song1_status == false){
+        song1.stop(); 
+        document.getElementById("song_name").innerHTML = "playing Sugar_(getmp3.pro).mp3"; 
     }
 
     if(scoreleftWrist > 0.2){
         circle(leftWristX, leftWristY, 20); 
         InNumberleftWristY = Number(leftWristY); 
         remove_decimals = floor(InNumberleftWristY); 
+    }
+
+    song2_status = song2.isPlaying(); 
+
+    if(song2_status == true){
+        song2.play(); 
+        document.getElementById("song_name").innerHTML = "playing Heart_Of_Glass_(getmp3.pro).mp3"; 
+    }
+    if(song2_status == false){
+        song2.stop(); 
+        document.getElementById("song_name").innerHTML = "playing Heart_Of_Glass_(getmp3.pro).mp3"; 
+    }
+
+    if(scorerightWrist > 0.2){
+        circle(rightWristX, rightWristY, 20); 
+        InNumberrightWristY = Number(rightWristY); 
+        remove_decimals = floor(InNumberrightWristY); 
         }
 
 
@@ -57,14 +80,16 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results); 
         scoreleftWrist = results[0].pose.keypoints[9].score; 
+        scorerightWrist = results[0].pose.keypoints[10].score; 
         console.log("scoreleftWrist = " + scoreleftWrist); 
+        console.log("scorerightWrist = " + scorerightWrist); 
 
         leftWristX = results[0].pose.leftWrist.x; 
         leftWristY = results[0].pose.leftWrist.y; 
-        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY); 
+        console.log("leftWristX = " + leftWristX + " leftWristY = "+ leftWristY); 
 
         rightWristX = results[0].pose.rightWrist.x; 
         rightWristY = results[0].pose.rightWrist.y; 
-        console.log("rightWristX = "+ rightWristX + " rightWristY = "+ rightWristY); 
+        console.log("rightWristX = " + rightWristX + " rightWristY = "+ rightWristY); 
     }
 }
